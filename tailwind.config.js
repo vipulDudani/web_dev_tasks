75eeb7753710ba44df72422bcf8d7654a9f9ef7f
vipulDudani <128981674+vipulDudani@users.noreply.github.com>
2023-10-19 18:30:11 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        stylish: ["Arvo", "serif"],
+      },
+    },
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "450px" },
+      // => @media (max-width: 639px) { ... }
+
+      ss: { max: "330px" },
+    },
+  },
+  plugins: [],
+};
